25e30375d18b0bff71d7837177fbdf85eaf1c661
Michael Gord <michael.l.gord@gmail.com>
2016-12-10 17:44:49 -0500

    get address and private key from username

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.get("/", function(req, res) {
 app.post("/wallet", function(req, res) {
 	var brainsrc = req.body.brainsrc;
 	console.log(brainsrc);
-	res.send("complete" + brainsrc);
+	var input = new Buffer(brainsrc);
+	var hash = bitcore.crypto.Hash.sha256(input);
+	var bn = bitcore.crypto.BN.fromBuffer(hash);
+	var pk = new bitcore.PrivateKey(bn).toWIF();
+	var addy = new bitcore.PrivateKey(bn).toAddress();
+	res.send("The Brain wallet of: " + brainsrc + "<br>Addy: " + addy + "<br>Private Key: " + pk);;
 });
 
 app.listen(3009, function () {
